3ad4e31bf79895671cab7a79c7993da13f95db88
Alinde Öst <ost.alinde@gmail.com>
2024-03-05 13:37:46 +0100

    added auth0 login

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { router } from "./Router";
 import { RouterProvider } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./pages/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Auth0Provider
+      domain="dev-3aw1g306endiokup.us.auth0.com"
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
+    ,
   </React.StrictMode>
 );
--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,11 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import LoginButton from "../components/LoginButton"
+import LogoutButton from "../components/LogoutButton"
 
 
 function App() {
  
-
+  const {isAuthenticated} = useAuth0()
   return (
     <>
      <h1>Startsidan</h1>
+     {isAuthenticated ? <LogoutButton/> : <LoginButton/>}
+     
     </>
   )
 }
